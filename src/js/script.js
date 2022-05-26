12ae539c502d704f46e1777f6b8e60be40cf1607
Doha Helmy <doha.helmy@hotmail.com>
2022-05-27 01:34:25 +0200

    turn nutrition collapse

--- src/js/script.js
+++ src/js/script.js
@@ -5,3 +5,17 @@ $('.main-slider').owlCarousel({
   dots: true,
   items: 1,
 })
+
+for (let i = 0; i < $(".collapsible").length; i++) {
+  $(".collapsible")[i].addEventListener("click", function () {
+    $(this).toggleClass("active");
+    let content = $(this).next();
+    if (content.height()) {
+      content.css('max-height', '0')
+      content.css('padding', '0')
+    } else {
+      content.css('max-height', '100%')
+      content.css('padding', '3rem')
+    }
+  });
+}
